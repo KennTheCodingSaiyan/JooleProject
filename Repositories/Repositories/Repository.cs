a08cddb9dd3b27580e882d24e068ee5fc0d1faf7
KennTheCodingSaiyan <jiananyu0011@gmail.com>
2019-05-07 17:10:13 -0400

    first commit

---
+++ Repositories/Repositories/Repository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Repositories.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        TEntity GetByID(int id);
+        void Insert(TEntity entity);
+
+        void Delete(TEntity entity);
+
+        void Update(TEntity entity);
+
+    }
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected readonly DbContext context;
+        public Repository (DbContext context)
+        {
+            this.context = context;
+        }
+        public void Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
