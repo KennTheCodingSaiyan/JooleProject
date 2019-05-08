4769d0c367e7e40ccfc36b7fb87f1acb22f1996e
Nanyan Jiang <ambbeerjiang@gmail.com>
2019-05-08 11:45:58 -0400

    all repositories added

---
+++ Repositories/Repositories/PropertyValueRepo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DAL.Models;
+using System.Data.Entity;
+
+namespace Repositories.Repositories
+{
+    public interface IPropertyValueRepo : IRepository<tblUser>
+    {
+
+    }
+
+    public class PropertyValueRepo : Repository<tblUser>, IPropertyValueRepo
+    {
+        public PropertyValueRepo(DbContext context) : base(context)
+        {
+
+        }
+    }
+}
