a834df1a452264369530af9ffd773a17bac09dc3
KennTheCodingSaiyan <jiananyu0011@gmail.com>
2019-05-10 11:04:04 -0400

    commit vm for product

---
+++ Joole_MVC_Team1/Models/ProductViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DAL.Models;
+using Services;
+
+namespace Joole_MVC_Team1.Models
+{
+    public class ProductViewModel
+    {
+
+        public ProductViewModel(int id)
+        {
+            tblProduct product = new Service().getProductService(id);
+
+        }
+
+        public string Manufacture { get; set; }
+        public string Series { get; set; }
+        public string Model { get; set; }
+
+        public string Commercial { get; set; }
+
+        public string Application { get; set; }
+        public string MountingLoaction { get; set; }
+        public string Accessories { get; set; }
+
+        public int ModelYear { get; set; }
+
+    }
+}
