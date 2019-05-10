8162c56a30be1e58ed7a0d0277fc5f883178d1e7
Yiwen Zhao <crazyyiwen2015@gmail.com>
2019-05-10 11:01:18 -0400

    5/10/2019 Yiwen Zhao upload 6 files LoginService.cs  LoginPage.cshtml  UserLoginController.cs cssdemo.css  script_demo.js

---
+++ Joole_MVC_Team1/Controllers/UserLoginController.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Yiwen Zhao 
+ *  5/6/2019
+ *  UserLoginController.cs
+ *  
+ */
+using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+using Services;
+
+namespace Joole_MVC_Team1.Controllers
+{
+    public class UserLoginController : Controller
+    {
+        // UserLogin
+        public ActionResult LoginPage(string userIDInfo, string passwordInfo)
+        {
+            // if all the inputs are null keep the login page
+            if((userIDInfo == null) && (passwordInfo == null))
+            {
+                return View();
+            }
+            Services.Service LoginTrueOrFalse = new Services.Service();
+            bool LoginDetermine = LoginTrueOrFalse.LoginService(userIDInfo, passwordInfo);
+            // based on the return bool value to choose which page to go
+            if (LoginDetermine == false)
+            {
+                return View();
+            }
+            else
+            {
+                // this part should be the redirection page
+                /* 
+                 
+                 */
+                return RedirectToAction("SearchPage", "Search");
+            }
+        }
+    }
+}
