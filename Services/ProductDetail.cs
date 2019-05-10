0c25f4d16ab72b94e74b88262693721a249be1bf
KennTheCodingSaiyan <jiananyu0011@gmail.com>
2019-05-10 14:17:04 -0400

    product details view created

--- Services/ProductDetail.cs
+++ Services/ProductDetail.cs
@@ -17,5 +17,41 @@ namespace Services
             return product;
 
         }
+
+        public string GetManufNameByID(int id)
+        {
+            return uow.manufacture.GetByID(id).Manufacturer_Name.ToString();
+        }
+
+        public List<tblPropertyValue> GetTblPropertyValueByProductID(int id)
+        {
+            List<tblPropertyValue> res = new List<tblPropertyValue>();
+            List<tblPropertyValue> temp = uow.propertyvalue.GetAll().ToList<tblPropertyValue>();
+
+            foreach (tblPropertyValue pv in temp)
+            {
+                if(Int32.Parse(pv.Product_ID.ToString()) == id)
+                {
+                    res.Add(pv);
+                }
+            }
+            return res;
+        }
+        public List<tblProperty> GetTblPropertiesByPropID(int id)
+        {
+            List<tblProperty> res = new List<tblProperty>();
+            List<tblProperty> temp = uow.property.GetAll().ToList<tblProperty>();
+
+            foreach (tblProperty p in temp)
+            {
+                if (p.Property_ID == id)
+                {
+                    res.Add(p);
+                }
+            }
+
+            return res;
+
+        }
     }
 }
